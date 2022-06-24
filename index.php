7d9a1334ac3ce8266f23c338f5c20e737e516265
tBrause <brause.torsten@gmail.com>
2022-06-24 03:29:54 +0200

    add style and new functions

--- index.php
+++ index.php
@@ -13,13 +13,21 @@ require("ini/inc.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MongoDB and PHP</title>
+    <style>
+        .name {
+            display: inline-block;
+            min-width: 100px;
+        }
+    </style>
 </head>
 
 <body>
     <main>
         <h1>MongoDB and PHP</h1>
         <?php
-        require("view/get-collection.php");
+        require("view/check-collection.php");
+        checkCollection($connection_cluster, $collection, $data, $error_msg);
+        #require("view/get-collection.php");
         #require("view/set-collection.php");
         ?>
     </main>
