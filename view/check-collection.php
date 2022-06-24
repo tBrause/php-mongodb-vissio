27c8600c34575cb358723b2660313502d67941f2
tBrause <brause.torsten@gmail.com>
2022-06-24 03:29:06 +0200

    add check-collection

---
+++ view/check-collection.php
@@ -0,0 +1,59 @@
+<?php
+# save single request
+if (!isset($correct)) {
+    die();
+}
+
+# Collection und Daten
+$collection = 'vissio';
+$data = 'device';
+
+########################
+function checkCollection($connection_cluster, $collection, $data, $error_msg)
+{
+    # Container
+    $container = '';
+
+    try {
+        # startet den Manager zum Aufbau einer Verbindung
+        $manager = new MongoDB\Driver\Manager($connection_cluster);
+
+        # Abfrage
+        $query = new MongoDB\Driver\Query([]);
+
+        # Auswahl der Collection und Daten / Dokumente
+        $cursor = $manager->executeQuery("" . $collection . '.' . $data . "", $query);
+        #var_dump((array)$cursor);
+
+        # Ergebnis, Daten zu Objekten
+        foreach ($cursor as $object) {
+
+            $array = (array)$object;
+
+            $container .= '<span class="name">Spalten :</span> ' . count($array) . '<br>';
+
+            foreach ($array as $key => $value) {
+                $container .= '<span class="name">' . $key . '</span>';
+                $container .= ' ' . $value . '<br>';
+            }
+
+            $container .= '<br><br>';
+        }
+    } catch (MongoConnectionExeption $error) {
+        var_dump($error);
+    }
+
+    # Ausgabe der Fehlermeldungen
+    if (count($error_msg) >= 1) {
+
+        echo '<h3>Fehlende Werte : ' . count($error_msg) . '</h3>';
+
+        foreach ($error_msg as $value) {
+            echo $value . '<br>';
+        }
+    }
+
+    # Ausgabe der Ergebnisse
+    echo '<h3>Ergebnisse :</h3>';
+    echo '' . $container . '<br>';
+}
